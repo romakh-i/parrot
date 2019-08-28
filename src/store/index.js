c2e55aa005238c1ceeb658d1e1bc9148a08876cf
romkh-i <ivanromah@gmail.com>
2019-08-28 20:53:58 +0300

    styles, reducer

--- src/store/index.js
+++ src/store/index.js
@@ -7,45 +7,34 @@ const initialState = fromJS({
   email: null,
   items: [],
   myItems: [],
+  categories: [],
 });
 
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case Types.AUTHORIZE:
+      console.warn('object', state)
+
       return state.set("isLoggedIn", true)
         .set("jwt", action.payload.jwt);
 
     case Types.UN_AUTHORIZE:
-      return { ...initialState };
+      return initialState;
 
     case Types.GET_USER:
-      return state;
-    // return {
-    //   ...state,
-    //   email: action.payload.email,
-    //   myItems: action.payload.items,
-    // };
+      return state.set("email", action.payload.email)
+        .set("myItems", fromJS(action.payload.items));
 
     case Types.GET_CATEGORIES:
-      return {
-        ...state,
-        categories: action.payload.categories,
-      };
+      return state.set("categories", fromJS(action.payload.categories));
 
     case Types.GET_ITEMS:
-      return {
-        ...state,
-        items: action.payload.items,
-      };
+      return state.set("items", fromJS(action.payload.items));
 
     case Types.CREATE_ITEM: {
-      const items = state.items;
-      items.push(action.payload.item);
+      const items = state.get("items").push(fromJS(action.payload.items));
 
-      return {
-        ...state,
-        items,
-      };
+      return state.set("myItems", items);
     }
 
     default:
