015b5938f671e8937afb741240017e41d2c81bb9
romkh-i <ivanromah@gmail.com>
2019-08-28 13:44:19 +0300

    config, sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AppRouter from './components/AppRouter';
-import './App.css';
+import './App.sass';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import './index.sass';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
